9d078005de8d3537739fa9840712fa5ff34346be
Amit Azulay <azulay7@gmail.com>
2023-07-03 15:10:35 +0300

    create

--- src/components/CreateBlog.js
+++ src/components/CreateBlog.js
@@ -1,5 +1,47 @@
+import { useState } from "react";
+
 const CreateBlog = () => {
-  return <div>Create Blog</div>;
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("");
+  return (
+    <div className="create">
+      <h2>Create Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+
+        <label>Blog body:</label>
+        <textarea
+          required
+          placeholder="Share your ideas"
+          value={body}
+          onChange={(e) => {
+            setBody(e.target.value);
+          }}
+        />
+
+        <label>Blog Author:</label>
+        <select
+          value={author}
+          onChange={(e) => {
+            setAuthor(e.target.value);
+          }}
+        >
+          <option value="amit">amit</option>
+          <option value="shelly">shelly</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
+    </div>
+  );
 };
 
 export default CreateBlog;
